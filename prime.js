0c9db821ff93226cc6342dff7041754e0aa918fa
Rutuvik Patil <you@example.com>
2022-11-05 17:00:01 +0530

    prime problem fixed

--- prime.js
+++ prime.js
@@ -1,8 +1,15 @@
-function primeCheck(num){
- if(num%2==0){
-    console.log("Even");
-   } else {
-    console.log("Odd");
+function primechecker(a){
+  let count=0;
+  for(i=1;i<=a;i++){
+    if(a%i==0){
+      count++;
+    }
+  }
+  if(count==2){
+    console.log(a,"is a Prime Number");
+  } else {
+    console.log(a,"is not a prime number")
+  }
 }
 
-}
+primechecker(6);
